21a40d8a62d07c586a8cc4ad5faefc4f4a8f32bc
Stasis Yukniavichus <stasys1996@gmail.com>
2022-11-01 23:41:48 +0000

    Add UserTest (need fix), UserTestData and MatcherFactory

---
+++ src/test/java/com/lunchvote/UserTestData.java
@@ -0,0 +1,40 @@
+package com.lunchvote;
+
+import com.lunchvote.model.Role;
+import com.lunchvote.model.User;
+
+import java.util.Collections;
+import java.util.Date;
+
+import static com.lunchvote.model.AbstractBaseEntity.START_SEQ;
+
+public class UserTestData {
+    public static final MatcherFactory.Matcher<User> USER_MATCHER = MatcherFactory.usingIgnoringFieldsComparator("registered", "roles");
+
+    public static final int USER_ID = START_SEQ;
+    public static final int USER2_ID = START_SEQ + 1;
+    public static final int USER3_ID = START_SEQ + 2;
+    public static final int ADMIN_ID = START_SEQ + 3;
+    public static final int GUEST_ID = START_SEQ + 4;
+    public static final int NOT_FOUND = 10;
+
+    public static final User user = new User(USER_ID, "User", "user@gmail.com", "password", Role.USER);
+    public static final User user2 = new User(USER2_ID, "User2", "user2@ygmail.com", "password", Role.USER);
+    public static final User user3 = new User(USER3_ID, "User3", "user3@gmail.com", "password", Role.USER);
+    public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN);
+    public static final User guest = new User(GUEST_ID, "Guest", "guest@gmail.com", "guest");
+
+    public static User getNew() {
+        return new User(null, "New", "new@gmail.com", "newPass",false, new Date(), Collections.singleton(Role.USER));
+    }
+
+    public static User getUpdated() {
+        User updated = new User(user);
+        updated.setEmail("update@gmail.com");
+        updated.setName("UpdatedName");
+        updated.setPassword("newPass");
+        updated.setEnabled(false);
+        updated.setRoles(Collections.singletonList(Role.ADMIN));
+        return updated;
+    }
+}
