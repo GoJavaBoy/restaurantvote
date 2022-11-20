76d00a54731808e978e05fad0eea18282a8679d3
Stasis Yukniavichus <stasys1996@gmail.com>
2022-11-20 22:13:06 +0000

    Add restaurant service

---
+++ src/main/java/com/lunchvote/service/RestaurantService.java
@@ -0,0 +1,43 @@
+package com.lunchvote.service;
+
+import com.lunchvote.model.Restaurant;
+import com.lunchvote.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+import static com.lunchvote.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
+public class RestaurantService {
+
+    private final RestaurantRepository repository;
+
+    public RestaurantService(RestaurantRepository repository) {
+        this.repository = repository;
+    }
+
+    public Restaurant create(Restaurant restaurant) {
+        Assert.notNull(restaurant, "restaurant must not be null");
+        return repository.save(restaurant);
+    }
+
+    public Restaurant get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public void update(Restaurant restaurant) {
+        Assert.notNull(restaurant, "restaurant must not be null");
+        checkNotFoundWithId(repository.save(restaurant), restaurant.id());
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public List<Restaurant> getAll() {
+        return repository.getAll();
+    }
+}
